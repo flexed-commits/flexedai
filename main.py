f35a34ff6b17fb83dfd1d7f868d433c8247c8e9b
flexed-commits <sm2828377@gmail.com>
2025-12-23 12:12:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -16,10 +16,20 @@ OWNER = {
     "username": ".flexed.",
     "pfp": "https://cdn.discordapp.com/avatars/1081876265683927080/a2671291fa7a3f13e03022eeeac15ef2.webp?size=2048",
     "id": "1081876265683927080",
+    "link": "https://discord.com/users/1081876265683927080/"
+    "friend": "https://discord.gg/XNNUR4Qn"
+    "bio": """
+Add me as a friend to contact me. Must have a mutual friend to get it done.\n
+Born <t:1265842320:R>\n
+Created 👑 Shivam’s Discord:\n
+https://discord.gg/bzePwKSDsp\n
+E-mail: flexed@zohomail.in"""
+    "gunslol": "https://guns.lol/flexedfr"
+    "favserv": "https://discord.gg/bzePwKSDsp"
 }
 
 # Explicit instructions in the system prompt to use the link
-SYSTEM_PROMPT = f"You are a helpful assistant. Owner: {OWNER['name']}. If asked for the owner's PFP or image, you MUST include this exact link: {OWNER['pfp']},. If asked for owner id then include this {OWNER['id']}. If asked for owner username then include this {OWNER['username']}. Be chill, explanatory, change your tone according to the recepient"
+SYSTEM_PROMPT = f"You are a helpful assistant. Owner: {OWNER['name']}. If asked for the owner's PFP or image, you MUST include this exact link: {OWNER['pfp']},. If asked for owner id then include this {OWNER['id']}. If asked for owner username then include this {OWNER['username']}. Be chill, explanatory, change your tone according to the recepient. If the user asks for owner's profile link then include {OWNER['link']}. If the user asks how can he add the owner, then say that you must have a mutual friend with him to get it done, also include this friend link {OWNER['friend']}. If the user asks for owner's bio then send this along with other request: {OWNER['bio']}. If user asks for owner's guns.lol profile then include: {OWNER['gunslol']}. If user wants to know owner's favorite Discord Server, then tell that it is 👑Shivam’s Discord, and send this invite link: {OWNER['favserv']}"
 
 intents = discord.Intents.default()
 intents.message_content = True 
